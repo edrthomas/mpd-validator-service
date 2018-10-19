843eef53556380d4cc6de5771ae5341ff78e9cd2
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2018-10-19 15:40:19 +0200

    [ADD] schema validation. schematron WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+# MPD Online Validator
+
+This web service validates the schema and the schematron rules against MPDs.
+
+This does NOT check segment formats, only the MPD document.
--- mpd_validator/app.py
+++ mpd_validator/app.py
@@ -6,14 +6,20 @@ MPD Validator service.
 import falcon
 
 from lxml import etree
+from lxml import isoschematron
 
 class MPDs(object):
     """
     Resource to handle MPDs.
     """
     def __init__(self):
-        self.schema = ""
-        self.schematron = ""
+        with open("./mpd_validator/schemas/"
+                  "DASH-MPD.xsd") as schema_file:
+            self.mpd_schema = etree.XMLSchema(etree.parse(schema_file))
+        #with open("./mpd_validator/schematrons/"
+        #          "schematron.xsd") as schematron_file:
+        #    self.mpd_schematron = isoschematron.Schematron(
+        #        etree.parse(schematron_file))
 
     def on_post(self, request, response):
         """
@@ -21,15 +27,23 @@ class MPDs(object):
         """
         if request.content_length:
             mpd = etree.parse(request.stream)
-            response.status = falcon.HTTP_200  # This is the default status
+            try:
+                self.mpd_schema.assert_(mpd)
+               # self.mpd_schematron.validate(mpd)
+                response.body = "Schema test: OK"
+                response.status = falcon.HTTP_200  # This is the default status
+            except Exception as error:
+                response.status = falcon.HTTP_400
+                response.body = error.message
         else:
-            response.status = falcon.HTTP_400  # This is the default status
+            response.body = "Empty request"
+            response.status = falcon.HTTP_400
 
 # falcon.API instances are callable WSGI apps
-APP = falcon.API()
+application = falcon.API()
 
 # Resources are represented by long-lived class instances
 mpds = MPDs()
 
 # things will handle all requests to the '/things' URL path
-APP.add_route('/mpds', mpds)
+application.add_route('/mpds', mpds)
